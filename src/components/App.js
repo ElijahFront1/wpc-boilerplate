1c00a37f5584efb1947a323f7a43985bc1beccd4
Span415 <64959991+Span415@users.noreply.github.com>
2021-09-11 22:10:57 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        console.log('https://medium.com/nuances-of-programming/%D0%BA%D0%B0%D0%BA-%D1%81-%D0%BD%D1%83%D0%BB%D1%8F-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D1%82%D1%8C-%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82-%D0%BD%D0%B0-react-%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D1%83%D1%8F-webpack-4-%D0%B8-babel-172c256d228')
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
